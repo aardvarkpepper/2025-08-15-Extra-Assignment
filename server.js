4f8b2f9a0324d2557b75939361fac9de3aee8cc4
James Lee <aardvarkpepper@gmail.com>
2025-08-16 05:11:32 -0400

    Add boss and destruction

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(express.json());
 
 let pseudoAPIKey = 'blur';
 
-const menuOptions = "'/' to see 'You have reached My amazing App?" + '<br/>' + "'/api/' to see 'Welcome to my API" + '<br/>' + "'/api/dashboard' to see 'This is the Admin Dashboard'" + '<br/>' + "'/api/profile' to see 'This is the API Profile Screen'" + '<br/>' + "'/api/jigglypuff' to see 'Tishana is correct, Jigglypuff IS the best Pokémon'" + '<br/>' + "=======================" + '<br/>' + "'/api/set' to activate the pseudo-API key" + '<br/>' + "'/api/remove' to deactivate the pseudo-API key" + '<br/>' + "'/api/locked' to see a different message depending on whether the pseudo-API key is activated or inactivated" + '<br/>' + "'/api/greetings/:userName' to see 'Hello, :userName!' (substitute a name for ':username')" + '<br/>' +"'/api/8ball' to battle a couple wild Pokemon because that's Pokéballs are like 8 balls, just go with it and have fun." // apparently the reading is set to HTML encoding or whatever, so \n escape characters don't work, but <br/> does.
+const menuOptions = "'/' to see 'You have reached My amazing App?" + '<br/>' + "'/api/' to see 'Welcome to my API" + '<br/>' + "'/api/dashboard' to see 'This is the Admin Dashboard'" + '<br/>' + "'/api/profile' to see 'This is the API Profile Screen'" + '<br/>' + "'/api/jigglypuff' to see 'Tishana is correct, Jigglypuff IS the best Pokémon'" + '<br/>' + "=======================" + '<br/>' + "'/api/set' to activate the pseudo-API key" + '<br/>' + "'/api/remove' to deactivate the pseudo-API key" + '<br/>' + "'/api/locked' to see a different message depending on whether the pseudo-API key is activated or inactivated" + '<br/>' + "'/api/greetings/:userName' to see 'Hello, :userName!' (substitute a name for ':username')" + '<br/>' +"'/api/8ball' to battle a couple wild Pokemon because that's Pokéballs are like 8 balls, just go with it and have fun." + '<br>' + "=======================" + '<br/>' + "'/api/imtheboss' to be the boss" + '<br>' + "'/api/imnottheboss' to resign as the boss" + '<br/>' + "'/api/boss/dashboard' to see the boss dashboard if you're the boss." + '<br/>' + "'/api/boss/destruction' to see boss destruction if you're the boss, but save this for when you're done with everything else."
+
+// apparently the reading is set to HTML encoding or whatever, so \n escape characters don't work, but <br/> does.
 
 const jigglypuff = { name: 'Jigglypuff', hitPoints: 60, attack: ['Bodyslam', 40] };
 const rattata = { name: 'Rattata', hitPoints: 30, attack: ['Tackle', 25] };
@@ -20,7 +22,21 @@ const assignPokemon = () => {
   currentPokemon = wildPokemon[randomIndex];
 }
 
-const menu = 
+let boss = false;
+let notDestructed = true;
+
+app.use((req, res, next) => {
+  if (notDestructed) {
+    next();
+  } else {
+    res.status(404).send("Apparently the boss decided to destroy everything.  Nothing a server reset wouldn't fix, though.  Somewhere in the distance you hear a Jigglypuff singing.");
+  }
+})
+
+const whosTheBoss = (req, res, next) => {
+  req['bossMode'] = boss;
+  next();
+}
 
 app.get('/', (req, res) => {
   res.send('You have reached My amazing App?' + '<br>' + '<br>' + menuOptions);
@@ -38,7 +54,7 @@ const setAPIKey = (req, res, next) => {
   next();
 }
 
-
+app.use(whosTheBoss);
 // app.use should not trigger on /, I think.
 app.use(myLoggerMiddleware);
 app.use(setAPIKey);
@@ -118,6 +134,33 @@ app.get('/api/8ball', (req, res) => {
   res.status(200).send(`Magic 8-ball says ${combat()}` + '<br>' + '<br>' + menuOptions);
 })
 
+app.get('/api/imtheboss', (req, res) => {
+  boss = true;
+  res.status(200).send(`You are now the boss.` + '<br>' + '<br>' + menuOptions);
+})
+
+app.get('/api/imnottheboss', (req, res) => {
+  boss = false;
+  res.status(200).send(`You are no longer the boss.` + '<br>' + '<br>' + menuOptions);
+})
+
+app.get('/api/boss/dashboard', (req, res) => {
+  console.log('api boss dash', req['bossMode']);
+  if (req['bossMode'] === true) {
+    res.status(200).send("You are the boss.  You enjoy a simple dashboard free of the complications of your everyday boss life.  You might think you could at least get dark mode as boss, but apparently Jigglypuff ran off with the implementation.  If you grow weary of being the boss, you can visit 'api/imnottheboss' to let Jigglypuff be the boss for the day." + '<br>' + '<br>' + menuOptions)
+  } else {
+    res.status(403).send("If you were the boss, you would have visited 'api/imtheboss' to turn on boss mode.  Good thing Jigglypuff doesn't have the manual dexterity to type on a character or Jigglypuff would be the boss . . ." + '<br>' + '<br>' + menuOptions)
+  }
+});
+
+app.get('/api/boss/destruction', (req, res) => {
+  if (req.bossMode) {
+    notDestructed = false;
+    res.status(200).send("You have decided to retire from the stresses of your everyday boss life with your friend and companion Jigglypuff.  As you set the destruction timer and walk away, you think to yourself 'That'll teach them not to implement dark mode!  Bwahahahaha!'")
+  } else {
+    res.status(403).send("If you were the boss, you would have visited 'api/imtheboss' to turn on boss mode.  Good thing Jigglypuff doesn't have the manual dexterity to type on a character or Jigglypuff would be the boss . . ." + '<br>' + '<br>' + menuOptions)
+  }
+});
 
 //Profile Screen", and D. /api/jigglypuff that shows "Tishana is correct, Jigglypuff IS the best Pokémon". REMEMBER to add an error message if person is FORBIDDEN
 // a /greetings route that takes the name in the URL and says "Hello, <person name>!"
